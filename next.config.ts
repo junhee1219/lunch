a706e9ab2968eea407b4efca17aee3c2daaacc37
JHLee <leejunhee1219@snu.ac.kr>
2025-05-25 16:10:55 +0900

    add: eslint 설정(for deployment)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    eslint: {
+        // 빌드 중 ESLint 오류가 나와도 계속 빌드하도록 함
+        ignoreDuringBuilds: true,
+    }
 };
 
 export default nextConfig;
