2e88a25d731e0fbb04848c5ad2c6f8a0d613f06b
jhlee3 <jhlee3@businessinsight.co.kr>
2025-05-27 11:28:42 +0900

    tailwind 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      'light', // 기본 테마도 포함
+      {
+        lunchapp: {
+          primary: '#10b981',      // emerald-500
+          secondary: '#3b82f6',    // blue-500
+          accent: '#f472b6',       // pink-400
+          neutral: '#f3f4f6',      // gray-100
+          'base-100': '#ffffff',   // white
+          'base-200': '#f9fafb',   // gray-50
+          'base-300': '#e5e7eb',   // gray-200
+        },
+      },
+    ],
+  }
 }
 
